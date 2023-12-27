b177591d718347580ecd85c128b24bc658bbc95c
jakelong1995 <hoanglong2579@gmail.com>
2023-12-27 17:07:56 +0700

    a

--- src/CreateEvent.jsx
+++ src/CreateEvent.jsx
@@ -1,10 +1,36 @@
-import React from "react";
-import EventName from "./EventName";
+import React, { useState } from "react";
 
 const CreateEvent = () => {
+  const [] = useState({
+    EventName: "",
+    StartDate: "",
+    EndDate: "",
+    TimeZone: "",
+    EventLocation: "",
+    RequireApproval: "",
+    Capacity: "",
+    Public: "",
+    Color: "",
+    Typeface: "",
+  });
+  const handleChange = (e) => {
+    const changedField = e.target.value;
+    const newValue = e.target.value;
+    console.log(changedField, newValue);
+  };
   return (
     <div>
-      <EventName />
+      <div>
+        <input
+          type="text"
+          placeholder="Event Name"
+          value={CreateEvent.EventName}
+          onChange={handleChange}
+          name="EventName"
+          id="eventname"
+        />
+        <button type="submit">Submit</button>
+      </div>
     </div>
   );
 };
