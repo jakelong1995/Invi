64707d558765c2f37ccda416f5b295f6b6b927bc
Hoang Thinh <hoangducthinh22@gmail.com>
2023-12-28 16:09:10 +0700

    Update Header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../common/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
