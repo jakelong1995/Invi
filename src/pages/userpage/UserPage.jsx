f8963a1effb234abd82829a3b6c29657fbbab324
jakelong1995 <hoanglong2579@gmail.com>
2024-01-02 17:07:44 +0700

    2/1/24

--- src/pages/userpage/UserPage.jsx
+++ src/pages/userpage/UserPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const UserPage = (props) => {
-  const formData = props.location?.state;
+const UserPage = () => {
+  const location = useLocation();
+  const [formData, setFormData] = useState(location.state);
+
+  useEffect(() => {
+    // Update formData if the location state changes
+    setFormData(location.state);
+  }, [location.state]);
 
   return (
     <div>
@@ -9,6 +16,7 @@ const UserPage = (props) => {
       {formData ? (
         <>
           <p>Event Name: {formData.eventName}</p>
+          {/* Display other form data as needed */}
         </>
       ) : (
         <p>No data available</p>
