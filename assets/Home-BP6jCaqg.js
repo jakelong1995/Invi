e9ed50c9974ff0587ac66bf9d8cbfcef4c42d838
Quang Le <lqcool2408@gmail.com>
2024-01-07 15:59:41 +0700

    Updates

--- assets/Home-BP6jCaqg.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-He_9FzD-.js";const t=()=>e.jsxs("div",{children:[e.jsx("div",{children:"Welcome to INVI"}),e.jsx("div",{children:localStorage.getItem("userName")})]});export{t as default};
---
+++ assets/Home-vHLkSbs9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-pHFTurlV.js";const r=()=>e.jsx("div",{children:"Home"});export{r as default};
