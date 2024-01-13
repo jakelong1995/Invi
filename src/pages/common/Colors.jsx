fd7b11cc87d72e547d658ee9d62866e8e684d29c
Hoang Thinh <hoangducthinh22@gmail.com>
2024-01-14 00:00:44 +0700

    Update based on main at 12h00 14/1

---
+++ src/pages/common/Colors.jsx
@@ -0,0 +1,11 @@
+export const colors = [
+  { name: "Gray", value: "#374151" },
+  { name: "Pink", value: "#db2777" },
+  { name: "Fuchsia", value: "#a21caf" },
+  { name: "Purple", value: "#7e22ce" },
+  { name: "Blue", value: "#2563eb" },
+  { name: "Green", value: "#16a34a" },
+  { name: "Yellow", value: "#eab308" },
+  { name: "Orange", value: "#c2410c" },
+  { name: "Red", value: "#b91c1c" },
+];
--- src/pages/event/create/ThemeColor.jsx
+++ src/pages/event/create/ThemeColor.jsx
@@ -1,68 +1,61 @@
-import React from "react";
-import { Col, ColorPicker, Divider, Row, Space } from "antd";
-const ThemeColor = () => (
-  <ColorPicker
-    styles={{
-      popupOverlayInner: {
-        width: 468 + 24,
-      },
-    }}
-    presets={[
-      {
-        label: "Recommended",
-        colors: [
-          "#000000",
-          "#F5222D",
-          "#FA8C16",
-          "#FADB14",
-          "#8BBB11",
-          "#52C41A",
-          "#13A8A8",
-          "#1677FF",
-          "#2F54EB",
-          "#722ED1",
-          "#EB2F96",
-        ],
-      },
-    ]}
-    panelRender={(_, { components: { Picker, Presets } }) => (
-      <div
-        className="custom-panel"
-        style={{
-          display: "flex",
-          width: 468,
-        }}
-      >
-        <div
-          style={{
-            flex: 1,
-          }}
-        >
-          <Presets />
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { colors } from "../../common/Colors";
+
+export default function ThemeColor({ selected, setSelected }) {
+  return (
+    <div>
+      <Listbox value={selected} onChange={setSelected} horizontal>
+        <div className="relative mt-1">
+          <Listbox.Button className="flex items-center justify-between gap-2 relative w-full cursor-pointer rounded py-1 pl-2 pr-8 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300">
+            <div className="flex gap-2">
+              <div
+                className="w-6 h-6 rounded-full"
+                style={{ backgroundColor: selected.value }}
+              ></div>
+              <div className="text-gray-600">Color</div>
+            </div>
+            <div className="font-medium" style={{ color: selected.value }}>
+              {selected.name}
+            </div>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-500"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="flex p-4 justify-between absolute mt-1 w-full overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {colors.map((color, colorIdx) => (
+                <Listbox.Option
+                  key={colorIdx}
+                  className={({ active }) =>
+                    `relative cursor-pointer select-none`
+                  }
+                  value={color}
+                >
+                  {({ selected }) => (
+                    <div
+                      className="w-6 h-6 rounded-full p-1"
+                      style={{
+                        backgroundColor: `${color.value}`,
+                        border: selected ? `2px solid ${color.value}` : "none",
+                      }}
+                    ></div>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
-        <Divider
-          type="vertical"
-          style={{
-            height: "auto",
-          }}
-        />
-        <div
-          style={{
-            width: 234,
-          }}
-        >
-          <Picker />
-        </div>
-      </div>
-    )}
-  >
-    <div className="flex gap-4 m-2 justify-between items-center">
-      <div className="flex gap-2 items-center">
-        <div className=" rounded bg-black w-6 h-6"></div>
-        <div>Color</div>
-      </div>
-      <div>Gray</div>
+      </Listbox>
     </div>
-  </ColorPicker>
-);
-export default ThemeColor;
+  );
+}
